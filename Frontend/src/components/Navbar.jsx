f084c6f61f2924d0020223ae7f318d63c71ad5c8
Ajeet Singh <ajeet@example.com>
2026-02-01 11:11:18 +0530

    home page setup

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -40,13 +40,13 @@ function Navbar() {
                 </Link>
               )}
 
-              <button
+              {/* <button
                 onClick={handleLogout}
                 className="flex cursor-pointer items-center gap-2 px-2 py-2 rounded-xl text-sm font-bold text-slate-500 hover:text-red-600 transition-all"
               >
                 <LogOut size={18} />
                 Logout
-              </button>
+              </button> */}
             </>
           ) : (
 
